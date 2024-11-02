e6381a8dceb255e9a2a4fee8c246bb8d51606e08
Hafizt Athallah <113577651+hfzt07@users.noreply.github.com>
2024-11-02 12:42:42 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/webGallery-01-11-24-3-/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
